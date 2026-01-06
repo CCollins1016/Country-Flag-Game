742912f7609c3c39d124772b6cf6cd3a47f83876
CCollins1016 <ccollins@bsd220.org>
2026-01-06 10:32:08 -0600

    Initial Commit

---
+++ Country Flag GAme/Country_Flag_GAmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Country_Flag_GAmeApp.swift
+//  Country Flag GAme
+//
+//  Created by Christian Collins on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct Country_Flag_GAmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
