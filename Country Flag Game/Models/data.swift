dcd45e566575916ae5ed92d1a32a66b53cd37ccb
CCollins1016 <ccollins@bsd220.org>
2026-01-16 10:50:20 -0600

    finished game manager

---
+++ Country Flag Game/Models/data.swift
@@ -0,0 +1,33 @@
+//
+//  data.swift
+//  Country Flag GAme
+//
+//  Created by Christian Collins on 1/14/26.
+//
+
+import Foundation
+
+struct Data {
+    let countries = [
+        "Angola",
+        "Austria",
+        "China",
+        "England",
+        "Estonia",
+        "France",
+        "Germany",
+        "Greece",
+        "India",
+        "Israel",
+        "Italy",
+        "Norway",
+        "Pakistan",
+        "Peru",
+        "Saudi Arabia",
+        "South Africa",
+        "Spain",
+        "Sweden",
+        "Ukraine",
+        "United States"
+    ]
+}
